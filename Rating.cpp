7f89923ada8db4a75858201c61e0540fa2ece3bd
SprDarkoi <sprdarkoi@His-Mac-mini.local>
2025-09-16 23:25:13 +0700

    error handling for rating

--- Rating.cpp
+++ Rating.cpp
@@ -3,20 +3,42 @@
 
 using namespace std;
 
+bool isValidRating(string rating) {
+    bool dotSeen = false;
+    if (rating.empty()) return false;
+
+    for (size_t i = 0; i < rating.size(); i++) {
+        if (rating[i] == '.') {
+            if (dotSeen) return false; // more than one dot
+            dotSeen = true;
+        } else if (!isdigit(rating[i])) {
+            return false; // not a digit or dot
+        }
+    }
+
+    if (stof(rating) > 5.0 || stof(rating) < 1.0) return false;
+    return true;
+}
+
 Rating::Rating(float stars, string name, string comment)
 :stars(stars), raterName(name), comment(comment) {};
 
 void Rating::rateObj(string username){
-        cout << "Star(s) (1-5): "; cin >> stars;
-        while (true){
-            if (stars <= 5.0 && stars >= 1.0) break;
+        string starStr;
+        cout << "Star(s) (1-5): "; cin >> starStr;
+        while (!isValidRating(starStr)){
             cout << "Enter the star(s) from 1 to 5" << endl;
-            cout << "Enter again: "; cin >> stars;
+            cout << "Enter again: "; cin >> starStr;
         }
+
+        stars = stof(starStr);
 
         cin.ignore();
         cout << "Enter your comment: "; getline(cin, comment);
-        raterName = username;  
+        while (comment.empty()){
+            cout << "Enter your comment again: "; getline(cin, comment);
+        }
+        raterName = username;
     }
 
 void Rating::showRating(){
